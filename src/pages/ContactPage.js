55cca374ee9b5a649ea20613c3323a3aa4414744
petro2323 <aleksandarpetronijevic65@gmail.com>
2024-09-13 12:51:53 +0200

    modified contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -17,8 +17,8 @@ export const ContactPage = () => {
       <MDBContainer className="py-5">
 
         <MDBRow>
-          <MDBCol lg="4">
-            <MDBCard className="mb-4">
+          <MDBCol lg="3">
+            <MDBCard className="mb-4 mt-5">
                 <MDBCardBody className="text-center">
                     <MDBCardImage
                     src={UserIcon}
@@ -33,16 +33,16 @@ export const ContactPage = () => {
                     </div>
 
                     <div className="d-flex justify-content-center mb-4">
-                    <a href="https://www.linkedin.com/in/dragana-petronijevic-21056331" target="_blank" className="mx-2">
+                    <a href="https://www.linkedin.com/in/dragana-petronijevic-21056331" target="_blank" className="mx-2 custom-icon">
                         <MDBIcon fab icon="linkedin fa-lg" style={{ color: '#0077b5' }} />
                     </a>
-                    <a href="viber://chat?number=38269507680" target="_blank" className="mx-2">
+                    <a href="viber://chat?number=38269507680" target="_blank" className="mx-2 custom-icon">
                         <MDBIcon fab icon="viber fa-lg" style={{ color: '#7360f2' }} />
                     </a>
-                    <a href="https://wa.me/38269507680" target="_blank" className="mx-2">
+                    <a href="https://wa.me/38269507680" target="_blank" className="mx-2 custom-icon">
                         <MDBIcon fab icon="whatsapp fa-lg" style={{ color: '#25D366' }} />
                     </a>
-                    <a href="tel:+38269507680" target="_blank" className="mx-2">
+                    <a href="tel:+38269507680" target="_blank" className="mx-2 custom-icon">
                         <MDBIcon fas icon="phone fa-lg" style={{ color: '#25D366' }} />
                     </a>
                     </div>
@@ -50,7 +50,7 @@ export const ContactPage = () => {
             </MDBCard>
             
           </MDBCol>
-          <MDBCol lg="8">
+          <MDBCol lg="9">
             <MDBCard className="mb-4">
               <MDBCardBody>
                 <MDBRow>
@@ -82,21 +82,31 @@ export const ContactPage = () => {
                 <hr />
                 <MDBRow>
                   <MDBCol sm="3">
-                    <MDBCardText>Adresa</MDBCardText>
+                    <MDBCardText>Adresa 1</MDBCardText>
                   </MDBCol>
                   <MDBCol sm="9">
                     <MDBCardText className="text-muted">Orjenski Bataljon 56A, Herceg Novi</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Adresa 2</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">Gavra Vukovića bb, Podgorica</MDBCardText>
                   </MDBCol>
                 </MDBRow>
               </MDBCardBody>
             </MDBCard>
 
             <MDBRow className="justify-content-center">
-              <MDBCol md="9">
+              <MDBCol md="6">
+              <h5 className="text-center mt-4">Orjenski Bataljon 56A</h5>
                 <MDBCard className="mb-4 mb-md-0">
-                    <MDBCardBody className="d-flex justify-content-center">
+                  <MDBCardBody className="d-flex justify-content-center">
                     <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, width: '100%', overflow: 'hidden', maxWidth: '100%' }}>
-                    <iframe
+                      <iframe
                         src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d183.9770595453906!2d18.53094198167317!3d42.456825150113374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDLCsDI3JzI0LjMiTiAxOMKwMzEnNTIuMiJF!5e0!3m2!1sen!2s!4v1726168729002!5m2!1sen!2s"
                         width="100%"
                         height="100%"
@@ -104,13 +114,31 @@ export const ContactPage = () => {
                         allowFullScreen=""
                         loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"
-                    />
+                      />
                     </div>
-                    </MDBCardBody>
+                  </MDBCardBody>
+                </MDBCard>
+              </MDBCol>
+
+              <MDBCol md="6">
+              <h5 className="text-center mt-4">Gavra Vukovića bb</h5>
+                <MDBCard className="mb-4 mb-md-0">
+                  <MDBCardBody className="d-flex justify-content-center">
+                    <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, width: '100%', overflow: 'hidden', maxWidth: '100%' }}>
+                      <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d309.4274788028898!2d19.260359914047655!3d42.453550548329225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134d95006be3577b%3A0x66ba31a13bd6f9c2!2sZgrada%20TRUDBENIK!5e0!3m2!1sen!2s!4v1726223795695!5m2!1sen!2s"
+                        width="100%"
+                        height="100%"
+                        style={{ border: 0, position: 'absolute', top: 0, left: 0 }}
+                        allowFullScreen=""
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                      />
+                    </div>
+                  </MDBCardBody>
                 </MDBCard>
               </MDBCol>
             </MDBRow>
-            
           </MDBCol>
         </MDBRow>
       </MDBContainer>
