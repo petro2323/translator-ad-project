e87c61d027c42e17d377498bf1848e16c0bbf431
petro2323 <aleksandarpetronijevic65@gmail.com>
2024-09-14 11:38:08 +0200

    added not found page

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,6 +1,15 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+import {
+    MDBCol,
+    MDBContainer,
+    MDBRow,
+    MDBCard,
+    MDBCardText,
+    MDBCardBody
+  } from 'mdb-react-ui-kit';
+
 export const NotFoundPage = () => {
     const location = useLocation();
     
@@ -9,8 +18,32 @@ export const NotFoundPage = () => {
       }, [location.pathname]);
     
     return (
-        <>
-        <h1>Page Not Found</h1>
-        </>
+        <section style={{ backgroundColor: '#eee', height: '75vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <MDBContainer className="py-5">
+                <MDBRow className="justify-content-center align-items-center" style={{ height: '100%' }}>
+                <MDBCol lg="4">
+                    <MDBCard>
+                    <MDBCardBody className="d-flex flex-column justify-content-center align-items-center" style={{ minHeight: '200px' }}>
+                        <MDBRow className="text-center">
+                        <MDBCol><h1>Greška 404</h1></MDBCol>
+                        </MDBRow>
+                        <MDBRow className="text-center mt-3">
+                        <MDBCol>
+                            <MDBCardText className="text-muted">Stranica nije pronađena</MDBCardText>
+                        </MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBCol>
+                            <div className="d-flex justify-content-center mt-4">
+                                <a href="/" class="btn btn-outline-primary">Glavna stranica</a>
+                            </div>
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        </section>
     );
 }
