67fde31d1d94c46a09e3320e506b0c50f9d6b3d1
petro2323 <aleksandarpetronijevic65@gmail.com>
2026-02-14 16:56:25 +0100

    fix: url redirect

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+
+if (redirect) {
+  window.history.replaceState(null, null, redirect);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
